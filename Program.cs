566c01ddcf40bdbd35c186a2a2e2e4c9c8bb9657
Prasad Bhalerao <prasad.bhalerao985@gmail.com>
2022-06-13 14:44:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace DbDifferenceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateDeltaScript();
+
+            var deltaObjects = GetDeltaObjects();
+
+            if (deltaObjects.Count > 0)
+                SendDeltaEmail(deltaObjects);
+            else
+                Console.WriteLine("No difference found.");
+
+
+            Console.WriteLine("Process is completed.");
+            Console.ReadKey();
+        }
+
+        private static void SendDeltaEmail(List<string> deltaObjects)
+        {
+            try
+            {
+                Console.WriteLine("Sending email notification...");
+
+                var smtpService = new SMTPMailSenderService();
+                var smtpServerAddress = ConfigurationManager.AppSettings["SMTPServerAddress"];
+                var smtpServerPort = int.Parse(ConfigurationManager.AppSettings["SMTPServerPort"]);
+                var from = ConfigurationManager.AppSettings["From"];
+                var to = ConfigurationManager.AppSettings["To"];
+                var subject = ConfigurationManager.AppSettings["Subject"] + " " + DateTime.UtcNow; // DateTime.UtcNow.ToString("dd/MM/yyyy");
+                var body = GetEmailBody(deltaObjects);
+
+                var task = Task.Run(() => smtpService.SendMessage(from, to, subject, body, true, smtpServerAddress, smtpServerPort));
+                   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Sending email notification task failed.");
+            }
+        }
+
+        private static string GetEmailBody(List<string> deltaObjects)
+        {
+            // Load XML document
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            path = path.Substring(6);
+            path += "\\EmailTemplate.xml";
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+
+            var xmlBody = doc.ChildNodes[1].OuterXml;
+
+            var deltaObjectIntegratedXML = FormDeltaXml(xmlBody, deltaObjects);
+
+            var sourceDB = ConfigurationManager.AppSettings["SourceDbArgs"];
+            var sourceAttributeIndex = sourceDB.IndexOf("Server");
+            sourceDB = sourceDB.Substring(sourceAttributeIndex, (sourceDB.IndexOf("/tf") - sourceAttributeIndex));
+            
+            var targetDB = ConfigurationManager.AppSettings["TargetDbArgs"];
+            sourceAttributeIndex = targetDB.IndexOf("Server");
+            targetDB = targetDB.Substring(sourceAttributeIndex, (targetDB.IndexOf("/tf") - sourceAttributeIndex));
+
+            deltaObjectIntegratedXML = deltaObjectIntegratedXML.Replace("{0}", sourceDB);
+            deltaObjectIntegratedXML = deltaObjectIntegratedXML.Replace("{1}", targetDB);
+            deltaObjectIntegratedXML = deltaObjectIntegratedXML.Replace("\r\n", "");
+
+            return deltaObjectIntegratedXML;
+        }
+
+        private static string FormDeltaXml(string xmlBody, List<string> deltaObjects)
+        {
+            var tableData = "<td>";
+            var sequenceNumber = 0;
+            foreach (var item in deltaObjects.Distinct())
+            {
+                tableData += "<tr>";
+                tableData += "<td>" + ++sequenceNumber + "</td>";
+                tableData += "<td>" + "Stored Procedure" + "</td>";
+                tableData += "<td>" + item + "</td>";
+                //tableData += "<td>" + "To be taken" + "</td>";
+                tableData += "</tr>";
+            }
+            tableData += "</td>";
+            xmlBody = xmlBody.Replace("{2}", tableData);
+            return xmlBody;
+        }
+
+        private static List<string> GetDeltaObjects()
+        {
+            Console.WriteLine("Extracting delta objects from script...");
+            var outputFile = ConfigurationManager.AppSettings["OutputFilePath"];
+            var fileData = System.IO.File.ReadAllLines(outputFile);
+
+            var deltaObjects = fileData.Where(x => x.Contains("Altering") || x.Contains("Creating") || x.Contains("Dropping"))
+                .Select(x => x.Substring(17, x.Length - 22)).ToList();
+
+            return deltaObjects;
+        }
+
+        private static void GenerateDeltaScript()
+        {
+            var sqlPackagePath = ConfigurationManager.AppSettings["SQLPackagePath"];
+            try
+            {
+                Console.WriteLine("\nGenerating dacpac and delta script...");
+                var startInfo = new ProcessStartInfo();
+                startInfo.WorkingDirectory = sqlPackagePath;
+                startInfo.FileName = @"sqlpackage.exe";
+                startInfo.Arguments = ConfigurationManager.AppSettings["SourceDbArgs"];
+                startInfo.CreateNoWindow = false;
+                var process1 = Process.Start(startInfo);
+
+                startInfo.Arguments = ConfigurationManager.AppSettings["TargetDbArgs"];
+                var process2 = Process.Start(startInfo);
+
+                process1.WaitForExit();
+                process2.WaitForExit();
+
+                startInfo.Arguments = ConfigurationManager.AppSettings["DeltaCommandArgs"];
+                var process3 = Process.Start(startInfo);
+                process3.WaitForExit();
+                Console.WriteLine("\nFinished with delta generation.");
+            }
+            catch (Win32Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SMTPMailSenderService.cs
@@ -0,0 +1,68 @@
+﻿/**********************************************************************************
+Veritas: Copyright (c) 2017 Veritas Technologies LLC. All rights reserved.
+EV29-2660-5195-04-15-1
+**********************************************************************************/
+namespace DbDifferenceGenerator
+{
+    using System;
+    using System.Net.Mail;
+    using System.Threading.Tasks;
+
+
+    /// <summary>
+    /// Class used to implement the SendMailMEssages.
+    /// </summary>
+    public class SMTPMailSenderService
+    {
+        /// <summary>
+        /// Creates the mail addres.
+        /// </summary>
+        /// <param name="address">The address.</param>
+        /// <returns></returns>
+        public MailAddress CreateMailAddres(string address)
+        {
+            return new MailAddress(address);
+        }
+
+        public async Task SendMessage(MailMessage msg, string smtpServer, int port)
+        {
+            var smtpClient = new SmtpClient(smtpServer, port);
+            
+            // SMTP server sometimes does not seem to respond on first try.
+            // So, do retries with more waiting time on each loop
+            const int maxRetries = 3;
+            for (var i = 0; i <= maxRetries; i++)
+            {
+                try
+                {
+                    var loopDelay = i * 1000;
+                    if (loopDelay > 0)
+                        System.Threading.Tasks.Task.Delay(1500 + loopDelay).Wait();
+                    
+                    await smtpClient.SendMailAsync(msg);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    if (i == maxRetries)
+                        throw;
+                }
+            }
+        }
+
+        public async Task SendMessage(string fromAddress, string toAddress, string subject, string body, bool html, string smtpServer, int port)
+        {
+            using (var msg = new MailMessage())
+            {
+                msg.From = new MailAddress(fromAddress);
+                msg.To.Add(new MailAddress(toAddress));
+                msg.Subject = subject;
+                msg.Body = body;
+                msg.IsBodyHtml = html;
+
+                await SendMessage(msg, smtpServer, port);
+            }
+        }
+
+    }
+}
